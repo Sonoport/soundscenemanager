b76652755bceb3beda83725cd9f073b1993759d8
Chinmay Pendharkar <notthetup@gmail.com>
2015-04-08 16:54:40 +0800

    added maxVolume and fixed mute, scene transition

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function SoundSceneManager(options){
 		this._context = new AudioContext();
 	}
 
-	this._scenes = options.scenes;
+	this._scenes = [];
 	this._sceneFaders = [];
 	this._preMuteScene;
 	this._previousFadeStart;
@@ -20,7 +20,7 @@ function SoundSceneManager(options){
 	this.currentScene;
 	this.isMuted = false;
 
-	this._scenes.forEach(function(thisScene){
+	options.scenes.forEach(function(thisScene){
 		this.addScene(thisScene);
 		if(thisScene.name === options.startingScene){
 			this.currentScene = thisScene;
@@ -32,11 +32,13 @@ function SoundSceneManager(options){
 
 SoundSceneManager.prototype.addSoundToScene = function(thisSound, thisSceneName){
 	var selectedScene;
+
 	this._scenes.forEach(function (thisScene){
 		if (thisScene.name === thisSceneName){
 			selectedScene = thisScene;
 		}
 	}.bind(this));
+
 	if(!selectedScene){
 		console.warn("Scene", thisSceneName, " not found.");
 		return
@@ -48,6 +50,10 @@ SoundSceneManager.prototype.addSoundToScene = function(thisSound, thisSceneName)
 }
 
 SoundSceneManager.prototype.addScene = function(thisScene){
+	if (!thisScene.maxVolume || typeof thisScene.maxVolume !== 'number'){
+		thisScene.maxVolume = 100;
+	}
+	this._scenes.push(thisScene);
 	var newFader = new Fader(this._context);
 	thisScene.fader = newFader;
 	this._sceneFaders.push(newFader);
@@ -58,6 +64,7 @@ SoundSceneManager.prototype.addScene = function(thisScene){
 }
 
 SoundSceneManager.prototype.transitionToScene = function(sceneName, startTime, duration){
+
 	var nextScene;
 	if (!startTime || typeof startTime !== 'number'){
 		startTime = this._context.currentTime;
@@ -72,40 +79,49 @@ SoundSceneManager.prototype.transitionToScene = function(sceneName, startTime, d
 		}
 	});
 
+	if (!nextScene){
+		console.warn("Unknown next scene.")
+		return;
+	}
+
 	if (nextScene === this.currentScene){
 		console.warn("Current scene and next scene are the same. No transition done.")
 		return;
 	}
 
-	var fadeStartTime = Math.max(startTime - duration/2, this._context.currentTime);
-	var fadeEndTime = fadeStartTime + duration;
+	if (!this.isMuted){
+		var fadeStartTime = Math.max(startTime - duration/2, this._context.currentTime);
+		var fadeEndTime = fadeStartTime + duration;
 
-	if (this.currentScene){
-		console.log(""+this._context.currentTime, ": fading out", this.currentScene.name, ":", fadeStartTime, "-",fadeEndTime);
+		if (this.currentScene){
+			console.log(""+this._context.currentTime, ": fading out", this.currentScene.name, ":", fadeStartTime, "-",fadeEndTime);
 
 
-		var currentValue = 100;
-		var remainingTime = (this._previousFadeEnd-this._context.currentTime);
-		if (nextScene && remainingTime >0){
-			currentValue = (1-(remainingTime/(this._previousFadeEnd-this._previousFadeStart)))*100;
-			// console.log(currentValue);
+			var currentValue = this.currentScene.maxVolume;
+			var remainingTime = (this._previousFadeEnd-this._context.currentTime);
+			if (remainingTime >0){
+				currentValue = (1-(remainingTime/(this._previousFadeEnd-this._previousFadeStart)))*currentValue;
+				// console.log(currentValue);
+			}
+
+			this.currentScene.fader.volume.cancelScheduledValues(this._context.currentTime);
+			this.currentScene.fader.volume.setValueAtTime(currentValue,fadeStartTime);
+			this.currentScene.fader.volume.linearRampToValueAtTime(0,fadeEndTime);
 		}
 
-		this.currentScene.fader.volume.cancelScheduledValues(this._context.currentTime);
-		this.currentScene.fader.volume.setValueAtTime(currentValue,fadeStartTime);
-		this.currentScene.fader.volume.linearRampToValueAtTime(0,fadeEndTime);
+		if (nextScene){
+			console.log(""+this._context.currentTime, ": fading in", nextScene.name, ":", fadeStartTime, "-",fadeEndTime)
+			nextScene.fader.volume.cancelScheduledValues(this._context.currentTime);
+			nextScene.fader.volume.setValueAtTime(0,fadeStartTime);
+			nextScene.fader.volume.linearRampToValueAtTime(nextScene.maxVolume,fadeEndTime);
+		}
+
+		this._previousFadeStart = fadeStartTime;
+		this._previousFadeEnd = fadeEndTime;
 	}
 
-	if (nextScene){
-		console.log(""+this._context.currentTime, ": fading in", nextScene.name, ":", fadeStartTime, "-",fadeEndTime)
-		nextScene.fader.volume.cancelScheduledValues(this._context.currentTime);
-		nextScene.fader.volume.setValueAtTime(0,fadeStartTime);
-		nextScene.fader.volume.linearRampToValueAtTime(100,fadeEndTime);
-	}
-
-	this._previousFadeStart = fadeStartTime;
-	this._previousFadeEnd = fadeEndTime;
 	this.currentScene = nextScene;
+
 }
 
 SoundSceneManager.prototype.transitionToNextScene = function(startTime, duration){
@@ -147,19 +163,56 @@ SoundSceneManager.prototype.endTransition = function(endTime){
 	}
 	if (nextScene){
 		nextScene.fader.volume.cancelScheduledValues(this._context.currentTime);
-		nextScene.fader.volume.setValueAtTime(100,endTime);
+		nextScene.fader.volume.setValueAtTime(nextScene.maxVolume,endTime);
 	}
 }
 
 SoundSceneManager.prototype.mute = function(startTime, duration){
-	this._preMuteScene = this.currentScene.name;
-	this.transitionToScene(null,startTime,duration);
+	if (!startTime || typeof startTime !== 'number'){
+		startTime = this._context.currentTime;
+	}
+	if (!duration || typeof duration !== 'number'){
+		duration = this.fadeDuration;
+	}
+
+	if (this.currentScene){
+		console.log(""+this._context.currentTime, ": muting", this.currentScene.name, ":", startTime, "-",startTime+duration);
+
+		var currentValue = this.currentScene.maxVolume;
+		var remainingTime = (this._previousFadeEnd-this._context.currentTime);
+		if (remainingTime >0){
+			currentValue = (1-(remainingTime/(this._previousFadeEnd-this._previousFadeStart)))*currentValue;
+			// console.log(currentValue);
+		}
+
+		this.currentScene.fader.volume.cancelScheduledValues(this._context.currentTime);
+		this.currentScene.fader.volume.setValueAtTime(currentValue,startTime);
+		this.currentScene.fader.volume.linearRampToValueAtTime(0,startTime+duration);
+
+		this._previousFadeStart = startTime;
+		this._previousFadeEnd = startTime+duration;
+	}
 	this.isMuted = true;
 }
 
 SoundSceneManager.prototype.unMute = function(startTime, duration){
-	this.transitionToScene(this._preMuteScene,startTime,duration);
-	this._preMuteScene = null;
+	if (this.currentScene){
+		if (!startTime || typeof startTime !== 'number'){
+			startTime = this._context.currentTime;
+		}
+		if (!duration || typeof duration !== 'number'){
+			duration = this.fadeDuration;
+		}
+
+		console.log(""+this._context.currentTime, ": unmuting", this.currentScene.name, ":", startTime, "-",startTime+duration)
+
+		this.currentScene.fader.volume.cancelScheduledValues(this._context.currentTime);
+		this.currentScene.fader.volume.setValueAtTime(0,startTime);
+		this.currentScene.fader.volume.linearRampToValueAtTime(this.currentScene.maxVolume,startTime+duration);
+
+		this._previousFadeStart = startTime;
+		this._previousFadeEnd = startTime+duration;
+	}
 	this.isMuted = false;
 }
 
